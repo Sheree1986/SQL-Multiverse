3e609b892b6e8bcb12a8ae61ce01186df5880c0a
Elvis <elvisgarcia93@gmail.com>
2022-08-25 03:31:27 -0400

    Add Update Code

--- demo.sql
+++ demo.sql
@@ -46,3 +46,19 @@ SELECT first_name, last_name FROM customers;
 
 -- You can also choose to only get a particular column based on a condition to narrow down or filter your query. 
 SELECT first_name, last_name FROM customers WHERE age > 24;
+
+-- You can update a record from your database table as well. It's like re-assigning a variable in JS
+UPDATE customers (age) 
+SET (
+    age = 35
+)
+WHERE first_name = "Fred";
+
+-- You can update as many records as you'd like to update
+UPDATE customers (first_name, last_name, age)
+SET (
+    first_name = "Bruce",
+    last_name = "Wayne",
+    age = 31
+)
+WHERE first_name = "Johnny";
