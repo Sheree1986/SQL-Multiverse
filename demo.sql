28ebc61b004589af47525199e6c982f650bcc4ed
Elvis <elvisgarcia93@gmail.com>
2022-08-25 03:19:19 -0400

    Add comments to User Table Creation

--- demo.sql
+++ demo.sql
@@ -1,9 +1,14 @@
--- Creating the table -- 
+-- Creating the table(s) -- 
 
 CREATE TABLE customers (
+    -- Property/Attribute Name DataType
     id INT NOT NULL,
     first_name VARCHAR(20),
     last_name VARCHAR(20),
     age INT,
+    -- Assigning the 'id' property to be the Primary Key of this table
     PRIMARY KEY (id)
 );
+
+
+
